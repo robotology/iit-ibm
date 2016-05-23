ef83ab288446fe95ed369ed323f54c8cbf40d7b7
Carlo Ciliberto <cciliber@gmail.com>
2016-05-23 12:02:28 +0200

    added missing file for visualize-data demo

---
+++ examples/send_inertial_data.js
@@ -0,0 +1,39 @@
+// hello.js
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+var yarp = require('../yarp');
+
+app.use(express.static('examples/images'));
+app.use(express.static('js'));
+
+app.get('/', function(req, res){
+  res.sendFile('send_inertial_data.html',{ root : __dirname});
+});
+
+app.get('/receive', function(req, res){
+  res.sendFile('receive_inertial_data.html',{ root : __dirname});
+});
+
+
+app.get('/receive2', function(req, res){
+  res.sendFile('receive_intertial_data_plus_three.html',{ root : __dirname});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+yarp.browserCommunicator(io);
+
+
+
+
+
+
+
