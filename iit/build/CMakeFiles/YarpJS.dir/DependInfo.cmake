b2a22de296779721f0cd9e7c3dddf8fa49871a40
GiuliaDAngelo <giulia.dangelo@iit.it>
2018-10-17 18:10:05 +0200

    add IBM-CEDAT application: w4r1

---
+++ iit/build/CMakeFiles/YarpJS.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_Bottle.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Bottle.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_BufferedPort.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_BufferedPort_Bottle.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Bottle.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_BufferedPort_Image.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Image.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_BufferedPort_Sound.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Sound.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_Image.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Image.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_Network.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Network.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_RPCPort.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_RPCPort.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/YarpJS_Sound.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Sound.cpp.o"
+  "/home/gdangelo/workspace/yarp.js/YarpJS/src/_YarpJS_SelfRegister.cpp" "/home/gdangelo/workspace/yarp.js/build/CMakeFiles/YarpJS.dir/YarpJS/src/_YarpJS_SelfRegister.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/gdangelo/.cmake-js/node-x64/v8.11.2/include/node"
+  "../node_modules/nan"
+  "/home/gdangelo/workspace/yarp/build/src/libYARP_conf/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_OS/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_sig/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_eigen/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_gsl/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_math/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_dev/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_name/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_manager/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_profiler/include"
+  "/home/gdangelo/workspace/yarp/src/libYARP_logger/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../YarpJS/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
