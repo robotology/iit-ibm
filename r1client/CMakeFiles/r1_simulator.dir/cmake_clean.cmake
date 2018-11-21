c3e7f2183aab34324a1c04d57cd718bb89a8b807
GiuliaDAngelo <giulia.dangelo@iit.it>
2018-11-21 18:18:15 +0100

    refactoring

---
+++ r1client/CMakeFiles/r1_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r1_simulator.dir/src/r1_simulator.cpp.o"
+  "r1_simulator.pdb"
+  "r1_simulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r1_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
