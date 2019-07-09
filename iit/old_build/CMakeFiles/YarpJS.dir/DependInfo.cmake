dfdb8c8e3bb6720d1d4370e0114da4b58afeca06
Giulia D'Angelo <giulia.dangelo@iit.it>
2019-07-09 11:31:07 +0200

    iit build files

---
+++ iit/old_build/CMakeFiles/YarpJS.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_Bottle.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Bottle.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_BufferedPort.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_BufferedPort_Bottle.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Bottle.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_BufferedPort_Image.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Image.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_BufferedPort_Sound.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Sound.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_Image.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Image.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_Network.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Network.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_RPCPort.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_RPCPort.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/YarpJS_Sound.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Sound.cpp.o"
+  "/home/giulia/workspace/iit-ibm/iit/YarpJS/src/_YarpJS_SelfRegister.cpp" "/home/giulia/workspace/iit-ibm/iit/build/CMakeFiles/YarpJS.dir/YarpJS/src/_YarpJS_SelfRegister.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../YarpJS/include"
+  "/home/giulia/workspace/yarp/src/libYARP_OS/include"
+  "/home/giulia/workspace/yarp/build/src/libYARP_conf/include"
+  "/home/giulia/workspace/yarp/src/libYARP_sig/include"
+  "/home/giulia/workspace/yarp/src/libYARP_dev/include"
+  "/home/giulia/workspace/yarp/src/libYARP_math/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ iit/old_build/CMakeFiles/YarpJS.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Bottle.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Bottle.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Image.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_BufferedPort_Sound.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Image.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Network.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_RPCPort.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/YarpJS_Sound.cpp.o"
+  "CMakeFiles/YarpJS.dir/YarpJS/src/_YarpJS_SelfRegister.cpp.o"
+  "YarpJS.pdb"
+  "YarpJS.node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YarpJS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
